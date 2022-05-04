bd407ddef25446d34464b4c2fb74c991fe4854d1
thomasop <tdss33@hotmail.com>
2022-05-04 09:26:11 +0200

    Add: Comment Entity and test #6

---
+++ src/Entity/Comment.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Ce champ est requis !')]
+    private $text;
+
+    #[ORM\Column(type: 'datetime')]
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime('now');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
---
+++ tests/Entity/CommentTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Comment;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class CommentTest extends KernelTestCase
+{
+    public function test()
+    {
+        $comment = (new Comment())
+        ->setText('gj')
+        ->setCreatedAt(new DateTime('2011-01-01T15:03:01.012345Z'));
+
+        self::bootKernel();
+        $error = self::getContainer()->get('validator')->validate($comment);
+        $this->assertCount(0, $error);
+    }
+
+    public function testContent()
+    {
+        $comment = new Comment();
+        $content = 'Test content';
+
+        $comment->setText($content);
+        $this->assertEquals('Test content', $comment->getText());
+    }
+
+    public function testDate()
+    {
+        $comment = new Comment();
+        $date = new DateTime('2011-01-01T15:03:01.012345Z');
+
+        $comment->setCreatedAt($date);
+        $this->assertEquals(new DateTime('2011-01-01T15:03:01.012345Z'), $comment->getCreatedAt());
+    }
+}
